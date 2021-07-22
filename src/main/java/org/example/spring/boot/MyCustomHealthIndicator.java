7541c078a171405e81b3e704c6d74ad87f3bb163
winkymonkey <pal.abhishek.2008@gmail.com>
2021-07-22 11:22:42 +0530

    refactor

--- src/main/java/org/example/spring/boot/MyCustomHealthIndicator.java
+++ src/main/java/org/example/spring/boot/MyCustomHealthIndicator.java
@@ -1,4 +1,4 @@
-package com.example.spring.boot;
+package org.example.spring.boot;
 
 import org.springframework.boot.actuate.health.AbstractHealthIndicator;
 import org.springframework.boot.actuate.health.Health;
